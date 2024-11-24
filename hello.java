81f3adf425443594556865c81a93ee91eb2a3172
Aditya Pophale <91986375+aditya1423@users.noreply.github.com>
2024-11-24 23:46:33 +0530

    Add files via upload
    DAY 1 :
    
    Today I learned JAVA basic syntax, variables,  data types, Comments, and Data type conversion.

---
+++ hello.java
@@ -0,0 +1,38 @@
+public class hello {
+    public static void main(String[] args){
+       System.out.println("Hello world java");
+       System.out.println(4+3);
+       float a=3.44f;
+       short student=1234;
+       int age =23;
+       int marks = 98;
+
+       char x ='a';
+       String aditya = "i want to learn java";
+       
+        System.out.println(aditya);
+
+       System.out.println(a);
+       System.out.println(student);
+
+     /* 
+      multiple line comments
+
+       System.out.println(age);
+       System.out.println(marks);
+       
+    */
+
+    //    implict conversion
+       int er= 33;
+       long d=er;
+       System.out.println(d);
+
+    //    explicit conversion
+       int ss = 190;
+       byte newage= (byte)ss;
+
+       System.out.println(newage);
+       
+    }
+}
