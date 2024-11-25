cc8f17b9abd1201eb907a8ca3eb3b38dd89d9026
Aditya Pophale <91986375+aditya1423@users.noreply.github.com>
2024-11-26 00:52:28 +0530

    Add files via upload
    Day 2!
    
    Today I learned about logical, relational, AND, and  OR, logical operators.

---
+++ operators.java
@@ -0,0 +1,17 @@
+
+
+public class operators{
+    
+    public static void main(String[] args) {
+        int a= 14;
+        int b= 2;
+
+        System.out.println(a+b);
+        System.out.println(a/b);
+        System.out.println(a-b);
+        System.out.println(a%b);
+        System.out.println(a*b);
+
+    }
+}
+ 
---
+++ realtion_oprators.java
@@ -0,0 +1,17 @@
+
+
+public class realtion_oprators {
+    public static void main(String[] args) {
+        
+        // -relation operators always return boolean values
+
+        int a=12;
+        int b= 13;
+        System.out.println(a>b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        
+    }
+}
